00ccc5e8e7b36eb27cde0ff3c65fa6e3a9eac06b
alaa-ashraf-namasha <132090324+alaa-ashraf-namasha@users.noreply.github.com>
2023-05-02 03:43:49 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,125 @@
+
+$(document).ready(function(){
+    $("#foundation").click(function(){
+    $(".pop1").show(400 );
+    $("pre").fadeIn(2000);
+    $("section").fadeTo(900,0.2);
+    });
+    })
+    $(document).ready(function(){
+    $(".close").click(function(){
+    $(".pop1").hide(400 );
+    $("section").fadeTo(800 , 1);
+    });
+    })
+
+
+$(document).ready(function(){
+    $("#Concealer").click(function(){
+    $(".pop2").show(400 );
+    $("pre").fadeIn(2000);
+    $("section").fadeTo(900,0.2);
+    });
+    })
+    $(document).ready(function(){
+    $(".close").click(function(){
+    $(".pop2").hide(400 );
+    $("section").fadeTo(900,1);
+    });
+    })
+
+
+$(document).ready(function(){
+    $("#blusher").click(function(){
+    $(".pop3").show(400 );
+    $("pre").fadeIn(2000);
+    $("section").fadeTo(900,0.2);
+    });
+    })
+    $(document).ready(function(){
+    $(".close").click(function(){
+    $(".pop3").hide(400 );
+    $("section").fadeTo(900,1);
+    });
+    })
+
+    
+$(document).ready(function(){
+    $("#contour").click(function(){
+    $(".pop4").show(400 );
+    $("pre").fadeIn(2000);
+    $("section").fadeTo(900,0.2);
+    });
+    })
+    $(document).ready(function(){
+    $(".close").click(function(){
+    $(".pop4").hide(400 );
+    $("section").fadeTo(900,1);
+    });
+    })
+
+$(document).ready(function(){
+    $("#Highlighter").click(function(){
+    $(".pop5").show(400 );
+    $("pre").fadeIn(2000);
+    $("section").fadeTo(900,0.2);
+    });
+    })
+    $(document).ready(function(){
+    $(".close").click(function(){
+    $(".pop5").hide(400 );
+    $("section").fadeTo(900,1);
+    });
+    })
+$(document).ready(function(){
+    $("#mascara").click(function(){
+    $(".pop6").show(400 );
+    $("pre").fadeIn(2000);
+    $("section").fadeTo(900,0.2);
+    });
+    })
+    $(document).ready(function(){
+    $(".close").click(function(){
+    $(".pop6").hide(400 );
+    $("section").fadeTo(900,1);
+    });
+    })
+$(document).ready(function(){
+    $("#Libstick").click(function(){
+    $(".pop7").show(400 );
+    $("pre").fadeIn(2000);
+    $("section").fadeTo(900,0.2);
+    });
+    })
+    $(document).ready(function(){
+    $(".close").click(function(){
+    $(".pop7").hide(400 );
+    $("section").fadeTo(900,1);
+    });
+    })
+$(document).ready(function(){
+    $("#Libgloss").click(function(){
+    $(".pop8").show(400 );
+    $("pre").fadeIn(2000);
+    $("section").fadeTo(900,0.2);
+    });
+    })
+    $(document).ready(function(){
+    $(".close").click(function(){
+    $(".pop8").hide(400 );
+    $("section").fadeTo(900,1);
+    });
+    })
+$(document).ready(function(){
+    $("#Eyeliner").click(function(){
+    $(".pop9").show(400 );
+    $("pre").fadeIn(2000);
+    $("section").fadeTo(900,0.2);
+    });
+    })
+    $(document).ready(function(){
+    $(".close").click(function(){
+    $(".pop9").hide(400 );
+    $("section").fadeTo(900,1);
+    });
+    })
